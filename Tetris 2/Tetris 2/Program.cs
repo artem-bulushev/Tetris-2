831b4bf01196f09d36c3ba6cb7b0a223973c5f5d
artem-bulushev <bulushiev93@mail.ru>
2025-06-05 00:43:02 +0300

    Добавлен класс Point

--- Tetris 2/Tetris 2/Program.cs
+++ Tetris 2/Tetris 2/Program.cs
@@ -1,22 +1,24 @@
-﻿Console.SetWindowSize(40, 30);
-Console.SetBufferSize(40, 30);
+﻿using System.Drawing;
+using Project = System.Drawing.Point;
 
-int x1 = 2;
-int y1 = 3;
-char c1 = '*';
-
-Drow(x1, y1, c1);
-
-int x2 = 2;
-int y2 = 3;
-char c2 = '#';
-
-Drow(x2, y2, c2);
-
-Console.ReadLine();
-
-static void Drow(int x, int y, char c)
+class Program
 {
-    Console.SetCursorPosition(x, y);
-    Console.Write(c);
+    {
+      Console.SetWindowSize(40, 30);
+      Console.SetBufferSize(40, 30);
+
+      Point p1 = new Point();
+      p1.x = 2;
+      p1.y = 3;
+      p1.c = '*';
+      p1.Draw();
+
+    Point p2 = new Point();
+    p2.x = 4;
+    p2.y = 5;
+    p2.c = '*';
+    p2.Draw();
+
+    Console.ReadLine();
+    }
 }
