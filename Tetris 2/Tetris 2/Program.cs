9d59bb60af7a84ffeee3e08ef4761b30067816ae
artem-bulushev <bulushiev93@mail.ru>
2025-06-04 23:32:02 +0300

    Вывели две точки на экран

--- Tetris 2/Tetris 2/Program.cs
+++ Tetris 2/Tetris 2/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿Console.SetWindowSize(40, 30);
+Console.SetBufferSize(40, 30);
 
-//Проект 
+int x1 = 2;
+int y1 = 3;
+char c1 = '*';
+
+Drow(x1, y1, c1);
+
+int x2 = 2;
+int y2 = 3;
+char c2 = '#';
+
+Drow(x2, y2, c2);
+
+Console.ReadLine();
+
+static void Drow(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(c);
+}
